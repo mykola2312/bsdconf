68c63c90fcd95b6b9899ebe672de3f50384fd8d1
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-09-08 19:03:06 +0300

    update win10 bhyve script

--- bhyve/win10.sh
+++ bhyve/win10.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+./enroll-tap.sh vnat0 tap0 &
+
 bhyve                                                     \
   -c 2                                                    \
   -s 0,hostbridge                                         \
-  -s 3,virtio-scsi,/var/lib/libvirt/images/win10.img      \
-  -s 4,ahci-cd,/home/mykola/iso/win10-eagle123.iso        \
-  -s 5,ahci-cd,/home/mykola/iso/virtio-win-0.1.215.iso    \
+  -s 3,ahci-hd,/var/lib/libvirt/images/win10.img          \
   -s 10,virtio-net,tap0                                   \
-  -s 15,fbuf,rfb=127.0.0.1:5900,w=1366,h=768              \
+  -s 15,fbuf,rfb=127.0.0.1:5900,w=1280,h=720              \
   -s 31,lpc                                               \
   -l com1,/dev/nmdm0A                                     \
   -l com2,/dev/nmdm1A                                     \
@@ -17,3 +17,4 @@ bhyve                                                     \
   -w                                                      \
   win10
 
+# -s 4,ahci-cd,/home/mykola/iso/win10-eagle123.iso        \
